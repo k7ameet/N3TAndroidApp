cb25f7f6ebc8a560b5b93183467cf18a1592f424
k7ameet <enterpriseswoof@gmail.com>
2018-06-10 03:03:09 +1200

    http

--- feature/src/main/java/com/example/n3t/n3tandroidapp/feature/AccelerometerService.java
+++ feature/src/main/java/com/example/n3t/n3tandroidapp/feature/AccelerometerService.java
@@ -3,7 +3,7 @@ package com.example.n3t.n3tandroidapp.feature;
 
 import android.app.Service;
 import android.content.Intent;
-import android.content.Context;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -12,19 +12,13 @@ import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
 
 
 public class AccelerometerService extends Service implements SensorEventListener{
@@ -34,9 +28,9 @@ public class AccelerometerService extends Service implements SensorEventListener
     private double x = 0;
     private double y = 0;
     private double z = 0;
-    private String url = "https://n3t-portal.herokuapp.com/postDataLocation";
-    private HashMap<String, String> hm;
-    private String jsonTest = "{\"IMU_x\":\"123\",\"IMU_y\":\"12\"\"IMU_z\":\"1\",\"dateTime\":\"9/6\",\"barometricPressure\":\"\",\"longitude\":\"50\",\"latitude\":\"51\",\"temperature\":\"\",\"windSpeed\":\"\",\"photo\":\"\",\"id\":\"123\"}";
+    private String urlString = "https://n3t-portal.herokuapp.com/postDataLocation";
+    private String urlTest = "";
+    private String jsonTest = "{\n\"IMU_x\":\"123\",\n\"IMU_y\":\"12\"\"IMU_z\":\"1\",\n\"dateTime\":\"9/6\",\n\"barometricPressure\":\"\",\n\"longitude\":\"50\",\n\"latitude\":\"51\",\n\"temperature\":\"\",\n\"windSpeed\":\"\",\n\"photo\":\"\",\n\"id\":\"123\"\n}";
 
     public AccelerometerService() {
     }
@@ -63,23 +57,10 @@ public class AccelerometerService extends Service implements SensorEventListener
                         try {
                             wait(5000);
                             Log.i("onStartCommand", "Service running");
-
-                            hm = new HashMap<String, String>();
-                            hm.put("IMU_x", "123");
-                            hm.put("IMU_y", "12");
-                            hm.put("IMU_z", "1");
-                            hm.put("dateTime", "9/6");
-                            hm.put("barometricPressure", "");
-                            hm.put("longitude", "50");
-                            hm.put("latitude", "51");
-                            hm.put("temperature", "");
-                            hm.put("windSpeed", "");
-                            hm.put("photo", "");
-                            hm.put("id", "123");
-
-
-                            sendUpdates(url, hm);
-                        }catch (java.lang.InterruptedException e){}
+                            sendUpdates(makeJsonObject(x, y, z));
+                        }catch (java.lang.InterruptedException e){
+                            Log.i("run", e.toString());
+                        }
                     }
                 }
             }
@@ -97,77 +78,51 @@ public class AccelerometerService extends Service implements SensorEventListener
     }
 
 
-    private String sendUpdates(String requestURL, HashMap<String, String> postDataParams) {
-        Log.i("accelerometer", x+" "+y+" "+z);
+    private void sendUpdates(JSONObject jsonObject) {
+        Log.i("accelerometer", x + " " + y + " " + z);
 
+        try {
+            URL url = new URL(urlString);
+            //URL url = new URL(urlTest);
 
-            URL url;
-            String response = "";
-            try {
-                url = new URL(requestURL);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
 
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+            httpURLConnection.setRequestProperty("Accept","application/json");
+            httpURLConnection.connect();
 
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(getPostDataString(postDataParams));
-
-                writer.flush();
-                writer.close();
-                os.close();
-                int responseCode=conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    String line;
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        response+=line;
-                    }
-                }
-                else {
-                    response="";
-
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return response;
+            DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+            //Log.i("JSON", jsonObject.toString());
+            wr.writeBytes(jsonObject.toString());
+            wr.flush();
+            wr.close();
+        } catch(Exception e) {
+            Log.i("Http post", e.toString());
+        }
     }
 
-    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-        for(Map.Entry<String, String> entry : params.entrySet()){
-            if (first) {
-                first = false;
-                result.append("{");
-            }
+    private JSONObject makeJsonObject(double x, double y, double z) {
+        JSONObject o = new JSONObject();
+        try {
+            o.put("IMU_x", x);
+            o.put("IMU_y", y);
+            o.put("IMU_z", z);
+            o.put("dateTime", "");
+            o.put("barometricPressure", "");
+            o.put("longitude", "");
+            o.put("latitude", "");
+            o.put("temperature", "");
+            o.put("windspeed", "");
+            o.put("photo", "");
+            o.put("id", 5);
 
-            result.append("\"" + entry.getKey() + "\"" + ":" + "\"" + entry.getValue() + "\",");
-            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        } catch (JSONException e) {
+            Log.i("json", e.toString());
         }
-        String res = removeLastChar(result.toString());
-        res = res + "}";
-
-        //return res;
-        return jsonTest;
-    }
-
-    public String removeLastChar(String s) {
-        if (s == null || s.length() == 0) {
-            return s;
-        }
-        return s.substring(0, s.length()-1);
+        return o;
     }
 
 
