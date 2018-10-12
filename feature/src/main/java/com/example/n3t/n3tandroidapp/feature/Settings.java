5efe18d4baad62214f6b74f567dc0f28efb8b383
k7ameet <enterpriseswoof@gmail.com>
2018-10-13 02:38:27 +1300

    12/10

--- feature/src/main/java/com/example/n3t/n3tandroidapp/feature/Settings.java
+++ feature/src/main/java/com/example/n3t/n3tandroidapp/feature/Settings.java
@@ -1,14 +1,28 @@
 package com.example.n3t.n3tandroidapp.feature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+
+    Button personalProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        personalProfile = (Button)findViewById(R.id.personal_profile);
+
+        personalProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Settings.this, PersonalProfile.class));
+            }
+        });
 
     }
 
