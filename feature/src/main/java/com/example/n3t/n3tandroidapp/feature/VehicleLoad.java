c8a7574334e64ed58764c63204f140a199f8385c
k7ameet <enterpriseswoof@gmail.com>
2018-10-13 00:14:56 +1300

    12/10

---
+++ feature/src/main/java/com/example/n3t/n3tandroidapp/feature/VehicleLoad.java
@@ -0,0 +1,13 @@
+package com.example.n3t.n3tandroidapp.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VehicleLoad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_load);
+    }
+}
